143d85ef1721560d7564b09b692920ad1c7fe032
Nikita Kozlov <kns@kns_pc>
2023-10-26 11:05:58 +0300

    прод

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,55 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
+import dotenv from "dotenv";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server:{
-    proxy:{
-      "/api":{
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path)=>path.replace(/^\/api/,"")
+
+let config;
+dotenv.config({path: ".env"});
+
+//npm install -g win-node-env для NODE_ENV на windows
+if (process.env.NODE_ENV === 'development') {
+  config = {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          secure: true,
+          rewrite: (path) => path.replace(/^\/api/, "")
+        }
+      },
+      port: 8080,
+      host: true,
+    }
+  };
+} else {
+  config = {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          secure: true,
+          rewrite: (path) => path.replace(/^\/api/, "")
+        }
+      },
+      port: 8080,
+      host: true,
+      https: {
+        key: fs.readFileSync(process.env.KEY),
+        cert: fs.readFileSync(process.env.CERT),
+        ca: [
+          fs.readFileSync(process.env.MDL),
+          fs.readFileSync(process.env.REQ),
+          fs.readFileSync(process.env.ROOT)
+        ]
       }
-    },
-    port: 8080,
-    host:true
-  }
-})
+    }
+  };
+}
+
+export default defineConfig(config);
 
